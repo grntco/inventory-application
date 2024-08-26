a3d5937629e1f0d644ddeb8fb1c946f823584f91
Grant Collins <grantcollins03@gmail.com>
2024-08-26 08:51:39 -0400

    set up app.js home route

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("node:path");
+const assetsPath = path.join(__dirname, "public");
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(assetsPath));
+
+app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
